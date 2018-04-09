f0790bfd4c674bde7af8dee73f5b9e397fe0c0ba
mikearbib <arbib.michael@gmail.com>
2018-04-09 22:23:08 +0200

    setup local conf

--- config/index.js
+++ config/index.js
@@ -40,7 +40,16 @@ module.exports = {
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
-    cssSourceMap: true
+    cssSourceMap: true,
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '/api': ''
+        },
+      },
+    }
   },
 
   build: {
