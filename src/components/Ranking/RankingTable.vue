2310455777b9d72d74ffb9908602129201daed33
mikearbib <arbib.michael@gmail.com>
2018-05-02 20:47:34 +0200

    created ranking

---
+++ src/components/Ranking/RankingTable.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <el-table
+      :data="tableData"
+      :default-sort = "{prop: 'points', order: 'descending'}"
+      style="width: 100%">
+      <!-- <el-table-column
+        prop="rank"
+        label="Rank"
+        width="100">
+      </el-table-column> -->
+      <el-table-column
+        prop="name"
+        label="Name"
+        align="left"
+        width="280">
+      </el-table-column>
+      <el-table-column
+        prop="victories"
+        label="Victories"
+        sortable
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="draws"
+        label="Draws"
+        sortable
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="losses"
+        label="Losses"
+        sortable
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="points"
+        label="Points"
+        sortable
+        width="100">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ranking-table',
+  components: {},
+  created() {
+    this.$store.dispatch('setRanking');
+  },
+  computed: {
+    tableData() {
+      return this.$store.getters.ranking;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
--- src/pages/RankingPage.vue
+++ src/pages/RankingPage.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <h3>{{$t('RANKING')}}</h3>
+    <ranking-table></ranking-table>
   </div>
 </template>
 
 <script>
+import RankingTable from '@/components/Ranking/RankingTable';
 
 export default {
   name: 'ranking-container',
-  components: {},
+  components: { RankingTable },
   created() {},
   computed: {},
 };
