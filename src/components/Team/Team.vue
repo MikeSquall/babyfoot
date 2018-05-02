be054c941187de68ed578feaab7f70da944df663
mikearbib <arbib.michael@gmail.com>
2018-05-02 20:47:00 +0200

    update teams

--- src/components/Team/Team.vue
+++ src/components/Team/Team.vue
@@ -1,37 +1,56 @@
 <template>
-  <div>
-    <el-card class="box-card">
-      <div slot="header" class="clearfix">
-        <span>{{ name }}</span>
+  <div class="card-container">
+    <transition-group name="flip" class="flipper">
+      <div class="front" v-if="!showCardBack" key="front">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>{{ name }}</span>
+            <el-button type="text"
+              style="float: right; padding: 0 3px;"
+              @click="flipCard">
+              {{ $t('PLAYERS') }}
+            </el-button>
+          </div>
+          <div>
+            <el-row :gutter="20">
+              <el-col :span="8">{{ $t('VICTORIES') }}</el-col>
+              <el-col :span="8">{{ $t('DRAWS') }}</el-col>
+              <el-col :span="8">{{ $t('LOSSES') }}</el-col>
+            </el-row>
+            <el-row :gutter="20">
+              <el-col :span="8">{{ victories }}</el-col>
+              <el-col :span="8">{{ draws }}</el-col>
+              <el-col :span="8">{{ losses }}</el-col>
+            </el-row>
+          </div>
+        </el-card>
       </div>
-      <table>
-        <tr>
-          <td>Victories</td>
-          <td>{{ victories }}</td>
-        </tr>
-        <tr>
-          <td>Draws</td>
-          <td>{{ draws }}</td>
-        </tr>
-        <tr>
-          <td>Losses</td>
-          <td>{{ losses }}</td>
-        </tr>
-        <tr>
-          <td rowspan="2">Players</td>
-          <td v-for="player in players" :key="player.id">
-            {{ player.first_name + ' ' + player.last_name }}
-          </td>
-        </tr>
-      </table>
-    </el-card>
+      <div class="back" v-if="showCardBack" key="back">
+        <el-card class="box-card">
+          <div slot="header">
+            <span>{{ $t('PLAYERS') }}</span>
+            <el-button type="text"
+              style="float: right; padding: 0 3px;"
+              @click="flipCard">
+                <icon name="arrow-left"></icon>
+              </el-button>
+          </div>
+          <div>
+            <team-players :players="players"></team-players>
+          </div>
+        </el-card>
+      </div>
+    </transition-group>
   </div>
 </template>
 
 <script>
+import TeamPlayers from '@/components/Team/TeamPlayers';
+import 'vue-awesome/icons/arrow-left';
 
 export default {
   name: 'team',
+  components: { TeamPlayers },
   props: {
     name: String,
     victories: Number,
@@ -39,12 +58,48 @@ export default {
     draws: Number,
     players: Array,
   },
-  components: {},
+  data() {
+    return {
+      showCardBack: false,
+    };
+  },
+  methods: {
+    flipCard() {
+      this.showCardBack = !this.showCardBack;
+    },
+  },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.card-container {
+  position: relative;
+}
+
+.flipper {
+  position: relative;
+}
+
+.front, .back {
+  backface-visibility: hidden;
+  height: 160px;
+}
+
 .box-card {
   margin-bottom: 20px;
+}
+
+.flip-enter,
+.flip-leave-to {
+  transform: rotateY(90deg);
+}
+
+.flip-enter-active {
+  transition: all 0.5s ease;
+  transition-delay: 0.5s;
+}
+
+.flip-leave-active {
+  transition: all 0.5s ease;
 }
 </style>
---
+++ src/components/Team/TeamPlayers.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-row :gutter="20" type="flex" justify="space-around">
+      <el-col
+        :span="12"
+        v-for="player in players"
+        :key="player.id"
+        class="team-player">
+        <div v-if="player.nickname">{{ player.nickname }}</div>
+        <div v-else>
+          <div>{{ player.first_name }}</div>
+          <div>{{ player.last_name }}</div>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'team-players',
+  props: {
+    players: Array,
+  },
+  components: {},
+};
+</script>
+
+<style lang="scss">
+.team-player {
+  display: flex;
+  flex-direction: column;
+}
+</style>
